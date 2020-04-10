351ad93520278dd0efb16431f00fff811aafee53
slaily <slavov.iliyan96@gmail.com>
2020-04-10 12:56:41 +0300

    Add warn for deprecation model fields using system checks - Speaker

--- project/apps/speaker/models.py
+++ project/apps/speaker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import checks
 
 
 class Speaker(models.Model):
@@ -14,3 +15,41 @@ class Speaker(models.Model):
         # Set explicit database table name to avoid
         # prefix name with the database name
         db_table = 'speakers'
+
+    @classmethod
+    def _first_name_field_warning_check(cls):
+        return checks.Warning(
+            (
+                'Field `first_name` is deprecated and will be '
+                'removed in the next version.',
+             ),
+            hint='Use field `full_name`.',
+            obj='speaker',
+            id='models.W301',
+        )
+
+    @classmethod
+    def _last_name_field_warning_check(cls):
+        return checks.Warning(
+            (
+                'Field `last_name` is deprecated and will be '
+                'removed in the next version.',
+             ),
+            hint='Use field `full_name`.',
+            obj='speaker',
+            id='models.W302',
+        )
+
+    @classmethod
+    def check(cls, **kwargs):
+        checks = super().check(**kwargs)
+        first_name_field_check = cls._first_name_field_warning_check()
+        last_name_field_check = cls._last_name_field_warning_check()
+        checks.extend(
+            [
+                first_name_field_check,
+                last_name_field_check
+            ]
+        )
+
+        return checks
