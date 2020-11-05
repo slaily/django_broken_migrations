8f2deb1dd62e54621495834d323ddc1d4ed50dc0
slaily <slavov.iliyan96@gmail.com>
2020-11-05 15:58:16 +0200

    Modify the model

--- project/apps/event/models.py
+++ project/apps/event/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+from project.apps.speaker.models import Speaker
+
 
 class Event(models.Model):
     title = models.CharField(max_length=254)
+    speaker = models.ForeignKey(
+        Speaker,
+        related_name='events',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         # Set explicit database table name to avoid
